bc23b244016dd271b4928be7ce39cd40c447a849
akechsalim <akechsalimwork@gmail.com>
2023-11-23 16:52:52 +0300

    changes

--- user-login-and-registration/src/main/java/com/akechsalim/userloginandregistration/security/config/WebSecurityConfig.java
+++ user-login-and-registration/src/main/java/com/akechsalim/userloginandregistration/security/config/WebSecurityConfig.java
@@ -1,32 +1,56 @@
 package com.akechsalim.userloginandregistration.security.config;
 
-import lombok.AllArgsConstructor;
+import com.akechsalim.userloginandregistration.appuser.AppUserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+    private final AppUserService appUserService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public WebSecurityConfig(AppUserService appUserService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.appUserService = appUserService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.
                 csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((requests) -> {
-                    requests.requestMatchers("/api/v*/registration/**")
-                            .permitAll()
-                            .anyRequest()
-                            .authenticated();
-                })
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/api/v*/registration/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
                 .formLogin(Customizer.withDefaults());
 
-        return (SecurityFilterChain) http.build();
+        return http.build();
     }
 
+    @Bean
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
+        provider.setUserDetailsService(appUserService);
+
+        return provider;
+    }
 
 }
